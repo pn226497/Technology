40c3955b5597d7b79d9c590ec57e11f10872a9e7
KimPhuong <pn22649782@gmail.com>
2023-06-11 19:20:20 +0700

    first commit

--- storeweb/src/cart/Cart.js
+++ storeweb/src/cart/Cart.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import data from '../data/data.json';
-import { useParams } from "react-router-dom";
+// import React, { useState, useEffect } from 'react';
+// import data from '../data/data.json';
+// import { useParams } from "react-router-dom";
 
 
-const Cart = () => {
-    const params = useParams();
+// const Cart = () => {
+//     const params = useParams();
 
-    return (
-        <div>
-            {data.products.map((data) => (
-                <div></div>
+//     return (
+//         <div>
+//             {data.products.map((data) => (
+//                 <div></div>
                   
 
 
-            ))}
+//             ))}
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
-export default Cart
+// export default Cart
